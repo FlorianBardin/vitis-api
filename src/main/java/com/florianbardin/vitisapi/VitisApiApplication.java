431f5a71c16c627e39313f40ad9d4097c242ea6e
Florian Bardin <fbardin51@gmail.com>
2025-12-28 19:38:14 +0100

    Initialization of the project

---
+++ src/main/java/com/florianbardin/vitisapi/VitisApiApplication.java
@@ -0,0 +1,13 @@
+package com.florianbardin.vitisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitisApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitisApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/florianbardin/vitisapi/VitisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florianbardin.vitisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitisApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
