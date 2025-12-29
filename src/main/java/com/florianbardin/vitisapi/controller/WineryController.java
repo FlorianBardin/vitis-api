42bb70369373a7be0ad167b7947da181e0715d4f
Florian Bardin <fbardin51@gmail.com>
2025-12-29 23:37:25 +0100

    Creation of a route to find a winery by id

--- src/main/java/com/florianbardin/vitisapi/controller/WineryController.java
+++ src/main/java/com/florianbardin/vitisapi/controller/WineryController.java
@@ -4,6 +4,7 @@ import com.florianbardin.vitisapi.entity.Winery;
 import com.florianbardin.vitisapi.repository.WineryRepository;
 import com.florianbardin.vitisapi.service.WineryService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,11 @@ public class WineryController {
     @GetMapping
     public List<Winery> getWineries() {
         return wineryService.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Winery findById(@PathVariable Integer id) {
+        return wineryService.findById(id);
     }
 
 }
