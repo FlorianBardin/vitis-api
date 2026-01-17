5e17b86dd8245ced57d992bf13b64a8fff15965f
Florian Bardin <fbardin51@gmail.com>
2026-01-17 15:14:00 +0100

    Creation of a new entity for wine

---
+++ src/main/java/com/florianbardin/vitisapi/entity/Wine.java
@@ -0,0 +1,111 @@
+package com.florianbardin.vitisapi.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Wine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer vintage;
+
+    @Column(nullable = false)
+    private Integer color;
+
+    private Double price;
+
+    private Integer stock;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Winery winery;
+
+    public Wine() {
+    }
+
+    public Wine(Integer id, String name, Integer vintage, Integer color, Double price, Integer stock, Winery winery) {
+        this.id = id;
+        this.name = name;
+        this.vintage = vintage;
+        this.color = color;
+        this.price = price;
+        this.stock = stock;
+        this.winery = winery;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getVintage() {
+        return vintage;
+    }
+
+    public void setVintage(Integer vintage) {
+        this.vintage = vintage;
+    }
+
+    public Integer getColor() {
+        return color;
+    }
+
+    public void setColor(Integer color) {
+        this.color = color;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Winery getWinery() {
+        return winery;
+    }
+
+    public void setWinery(Winery winery) {
+        this.winery = winery;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Wine wine = (Wine) o;
+        return Objects.equals(id, wine.id) && Objects.equals(name, wine.name) && Objects.equals(vintage, wine.vintage) && Objects.equals(color, wine.color) && Objects.equals(price, wine.price) && Objects.equals(stock, wine.stock) && Objects.equals(winery, wine.winery);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, vintage, color, price, stock, winery);
+    }
+}
