43507d7edb59ac7c6acd77c2c7a6ff3e7b9edd3d
Florian Bardin <fbardin51@gmail.com>
2026-01-20 00:08:09 +0100

    Updated the error that is thrown in the findById method

--- src/main/java/com/florianbardin/vitisapi/service/WineService.java
+++ src/main/java/com/florianbardin/vitisapi/service/WineService.java
@@ -2,8 +2,10 @@ package com.florianbardin.vitisapi.service;
 
 import com.florianbardin.vitisapi.entity.Wine;
 import com.florianbardin.vitisapi.repository.WineRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -23,8 +25,8 @@ public class WineService {
 
     public Wine findById(Integer id) {
         return wineRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException(
-                        "Wine with id " + id + " not found"
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Wine with id " + id + " not found"
                 ));
     }
 }
