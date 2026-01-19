7280bedbf4f787e3cf3c815936dff262cf9752dc
Florian Bardin <fbardin51@gmail.com>
2026-01-19 23:17:08 +0100

    Creation of the wine service

---
+++ src/main/java/com/florianbardin/vitisapi/service/WineService.java
@@ -0,0 +1,18 @@
+package com.florianbardin.vitisapi.service;
+
+import com.florianbardin.vitisapi.entity.Wine;
+import com.florianbardin.vitisapi.repository.WineRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WineService {
+
+    private final WineRepository wineRepository;
+
+    public WineService(WineRepository wineRepository) {
+        this.wineRepository = wineRepository;
+    }
+
+}
