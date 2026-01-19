75dbf647a856a0793a8ce8d3c90a78f3f38f38f3
Florian Bardin <fbardin51@gmail.com>
2026-01-20 00:06:57 +0100

    Added transactional annotation to optimize transaction with the wine repository

--- src/main/java/com/florianbardin/vitisapi/service/WineService.java
+++ src/main/java/com/florianbardin/vitisapi/service/WineService.java
@@ -3,10 +3,12 @@ package com.florianbardin.vitisapi.service;
 import com.florianbardin.vitisapi.entity.Wine;
 import com.florianbardin.vitisapi.repository.WineRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 public class WineService {
 
     private final WineRepository wineRepository;
