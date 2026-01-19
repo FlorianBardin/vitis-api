8fbfffcd65496706a896579a63c3d7e15549c27c
Florian Bardin <fbardin51@gmail.com>
2026-01-19 23:59:00 +0100

    Added transactional annotation to optimize transactions in the winery service

--- src/main/java/com/florianbardin/vitisapi/service/WineryService.java
+++ src/main/java/com/florianbardin/vitisapi/service/WineryService.java
@@ -3,10 +3,13 @@ package com.florianbardin.vitisapi.service;
 import com.florianbardin.vitisapi.entity.Winery;
 import com.florianbardin.vitisapi.repository.WineryRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 public class WineryService {
 
     private final WineryRepository wineryRepository;
@@ -26,14 +29,17 @@ public class WineryService {
                 ));
     }
 
+    @Transactional
     public void insertWinery(Winery winery) {
         wineryRepository.save(winery);
     }
 
+    @Transactional
     public void deleteWinery(Integer id) {
         wineryRepository.deleteById(id);
     }
 
+    @Transactional
     public void updateWinery(Integer id, Winery winery) {
         Winery updatedWinery = wineryRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException(
