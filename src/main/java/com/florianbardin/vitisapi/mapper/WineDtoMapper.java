36c49f0b33f6bdf448a1abece5ea5bc38cba4af1
Florian Bardin <fbardin51@gmail.com>
2026-01-21 20:21:37 +0100

    Added the method to convert a wine dto into a wine object

--- src/main/java/com/florianbardin/vitisapi/mapper/WineDtoMapper.java
+++ src/main/java/com/florianbardin/vitisapi/mapper/WineDtoMapper.java
@@ -6,5 +6,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class WineDtoMapper {
+    public Wine toWine(WineDto wineDto) {
+        Wine wine = new Wine();
+        wine.setName(wineDto.name());
+        wine.setVintage(wineDto.vintage());
+        wine.setColor(wineDto.color());
+        wine.setPrice(wineDto.price());
+        wine.setStock(wineDto.stock());
 
+        return wine;
+    }
 }
