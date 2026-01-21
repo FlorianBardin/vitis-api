6a6fb3fd3350fa50ecd2781eebaced50c032ee42
Florian Bardin <fbardin51@gmail.com>
2026-01-21 20:21:05 +0100

    Creation of the wine dto mapper class

---
+++ src/main/java/com/florianbardin/vitisapi/mapper/WineDtoMapper.java
@@ -0,0 +1,10 @@
+package com.florianbardin.vitisapi.mapper;
+
+import com.florianbardin.vitisapi.dto.WineDto;
+import com.florianbardin.vitisapi.entity.Wine;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WineDtoMapper {
+
+}
