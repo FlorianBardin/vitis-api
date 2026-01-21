698986423295acfeb0efccf7de802680d9ed2b81
Florian Bardin <fbardin51@gmail.com>
2026-01-21 19:38:48 +0100

    Creation of a DTO for the wine entity

---
+++ src/main/java/com/florianbardin/vitisapi/dto/WineDto.java
@@ -0,0 +1,25 @@
+package com.florianbardin.vitisapi.dto;
+
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record WineDto(
+        Integer id,
+        @NotBlank
+        String name,
+        @NotNull
+        @Digits(fraction = 0, integer = 4)
+        @PositiveOrZero
+        Integer vintage,
+        @NotBlank
+        String color,
+        @PositiveOrZero
+        Double price,
+        @PositiveOrZero
+        Integer stock,
+        @NotNull
+        Integer wineryId
+) {
+}
