41efac51a8b675623beed48ee8eedbe06fcb4c90
Florian Bardin <fbardin51@gmail.com>
2026-01-22 10:54:39 +0100

    Modified findAll method in wine service to implement WineDto

--- src/main/java/com/florianbardin/vitisapi/service/WineService.java
+++ src/main/java/com/florianbardin/vitisapi/service/WineService.java
@@ -1,6 +1,8 @@
 package com.florianbardin.vitisapi.service;
 
+import com.florianbardin.vitisapi.dto.WineDto;
 import com.florianbardin.vitisapi.entity.Wine;
+import com.florianbardin.vitisapi.mapper.WineDtoMapper;
 import com.florianbardin.vitisapi.repository.WineRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -8,6 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @Transactional(readOnly = true)
@@ -19,8 +23,11 @@ public class WineService {
         this.wineRepository = wineRepository;
     }
 
-    public List<Wine> findAll() {
-        return wineRepository.findAll();
+    public List<WineDto> findAll() {
+        return wineRepository.findAll()
+                .stream()
+                .map(WineDtoMapper::toWineDto)
+                .collect(toList());
     }
 
     public Wine findById(Integer id) {
