3617f8e0aaf8420c420dc7f8f1c14ba4ed80dfcf
Florian Bardin <fbardin51@gmail.com>
2026-01-25 00:56:17 +0100

    Created a method to transform a winery dto into a winery object in the mapper

--- src/main/java/com/florianbardin/vitisapi/mapper/WineryDtoMapper.java
+++ src/main/java/com/florianbardin/vitisapi/mapper/WineryDtoMapper.java
@@ -4,5 +4,14 @@ import com.florianbardin.vitisapi.dto.WineryDto;
 import com.florianbardin.vitisapi.entity.Winery;
 
 public class WineryDtoMapper {
+    public Winery toWinery(WineryDto wineryDto) {
+        Winery winery = new Winery();
 
+        winery.setId(wineryDto.id());
+        winery.setName(wineryDto.name());
+        winery.setRegion(wineryDto.region());
+        winery.setAddress(wineryDto.address());
+
+        return winery;
+    }
 }
