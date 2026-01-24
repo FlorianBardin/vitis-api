516cbcbeb8ddb2f6e612f5f3c691a3a50c841723
Florian Bardin <fbardin51@gmail.com>
2026-01-25 00:55:04 +0100

    Created a mapper class for the winery dto

---
+++ src/main/java/com/florianbardin/vitisapi/mapper/WineryDtoMapper.java
@@ -0,0 +1,8 @@
+package com.florianbardin.vitisapi.mapper;
+
+import com.florianbardin.vitisapi.dto.WineryDto;
+import com.florianbardin.vitisapi.entity.Winery;
+
+public class WineryDtoMapper {
+
+}
