c1274b5a68401ed5bc31ac26c381feb75bf21bba
Florian Bardin <fbardin51@gmail.com>
2026-01-26 23:11:12 +0100

    Extended wine repository to use the JpaSpecificationExecutor interface

--- src/main/java/com/florianbardin/vitisapi/wine/WineRepository.java
+++ src/main/java/com/florianbardin/vitisapi/wine/WineRepository.java
@@ -1,7 +1,8 @@
 package com.florianbardin.vitisapi.wine;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface WineRepository extends JpaRepository<Wine, Integer> {
+public interface WineRepository extends JpaRepository<Wine, Integer>, JpaSpecificationExecutor<Wine> {
     boolean existsByWineryId(Integer wineryId);
 }
