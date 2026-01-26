5fe93d3309670d828bef91f418a2a71ee94523dd
Florian Bardin <fbardin51@gmail.com>
2026-01-26 23:13:51 +0100

    Updated the color specification to be more efficient

--- src/main/java/com/florianbardin/vitisapi/wine/WineSpecs.java
+++ src/main/java/com/florianbardin/vitisapi/wine/WineSpecs.java
@@ -4,14 +4,20 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class WineSpecs {
     public static Specification<Wine> hasType(WineType wineType) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("type"), wineType);
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("type"), wineType);
     }
 
     public static Specification<Wine> hasColor(String color) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("color"), color);
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(
+                        criteriaBuilder.lower(root.get("color")),
+                        color.toLowerCase()
+                );
     }
 
     public static Specification<Wine> hasVintage(Integer vintage) {
-        return (root, query, criteriaBuilder) ->  criteriaBuilder.equal(root.get("vintage"), vintage);
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("vintage"), vintage);
     }
 }
