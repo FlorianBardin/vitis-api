47e72a99d90fee3e02f48254aae2d3dd51eb6ebb
Florian Bardin <fbardin51@gmail.com>
2026-01-26 23:06:49 +0100

    Created a specification to request wine by a type

--- src/main/java/com/florianbardin/vitisapi/wine/WineSpecs.java
+++ src/main/java/com/florianbardin/vitisapi/wine/WineSpecs.java
@@ -3,5 +3,7 @@ package com.florianbardin.vitisapi.wine;
 import org.springframework.data.jpa.domain.Specification;
 
 public class WineSpecs {
-
+    public static Specification<Wine> hasType(WineType wineType) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("type"), wineType);
+    }
 }
