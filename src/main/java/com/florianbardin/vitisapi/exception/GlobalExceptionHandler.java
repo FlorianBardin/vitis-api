6965192d3a41d8f8e38697f515b1ef4bd08fa259
Florian Bardin <fbardin51@gmail.com>
2026-01-28 19:16:57 +0100

    feat(exception): created a global exception handler class

---
+++ src/main/java/com/florianbardin/vitisapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.florianbardin.vitisapi.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
