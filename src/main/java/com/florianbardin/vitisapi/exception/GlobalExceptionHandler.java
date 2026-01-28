6acdc7d4e254b0dd3e2d5dc97a12ab8a703aefb7
Florian Bardin <fbardin51@gmail.com>
2026-01-28 19:18:54 +0100

    feat(exception): created a handler method for ResponseStatusException

--- src/main/java/com/florianbardin/vitisapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/florianbardin/vitisapi/exception/GlobalExceptionHandler.java
@@ -1,8 +1,23 @@
 package com.florianbardin.vitisapi.exception;
 
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ProblemDetail> handleResponseStatusException(ResponseStatusException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                e.getStatusCode(), e.getReason()
+        );
+        problemDetail.setProperty("timestamp", LocalDateTime.now());
+
+        return new ResponseEntity<>(problemDetail, e.getStatusCode());
+    }
 }
