f06b63952550b6bb9381865cc43d5398eadc2c32
Florian Bardin <fbardin51@gmail.com>
2026-01-29 19:03:46 +0100

    fix(wine): improved validation in the wine dto

--- src/main/java/com/florianbardin/vitisapi/wine/dto/WineDto.java
+++ src/main/java/com/florianbardin/vitisapi/wine/dto/WineDto.java
@@ -1,28 +1,33 @@
 package com.florianbardin.vitisapi.wine.dto;
 
 import com.florianbardin.vitisapi.wine.WineType;
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.*;
 
 public record WineDto(
         Integer id,
-        @NotBlank
+
+        @NotBlank(message = "Name is required")
+        @Size(max = 255, message = "Name must be less than 255 characters")
         String name,
-        @NotNull
-        @Digits(fraction = 0, integer = 4)
-        @PositiveOrZero
+
+        @NotNull(message = "Vintage year is required")
+        @Min(value = 1900, message = "Vintage must be later than 1900")
+        @Max(value = 2100, message = "Vintage must be a valid year (up to 2100)")
         Integer vintage,
-        @NotNull
+
+        @NotNull(message = "Wine type is required")
         WineType type,
-        @NotBlank
+
+        @NotBlank(message = "Color is required")
         String color,
-        @PositiveOrZero
+
+        @PositiveOrZero(message = "Price must be greater than or equal to 0")
         Double price,
-        @PositiveOrZero
+
+        @PositiveOrZero(message = "Stock must be greater than or equal to 0")
         Integer stock,
-        @NotNull
+
+        @NotNull(message = "Winery ID is required")
         Integer wineryId
 ) {
 }
