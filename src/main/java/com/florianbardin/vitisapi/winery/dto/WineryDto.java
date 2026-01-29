d8964d0c3d1b0f933d1a7ca52bfe6b8dbef72dd0
Florian Bardin <fbardin51@gmail.com>
2026-01-29 20:46:30 +0100

    fix(winery): improved validation in the winery dto

--- src/main/java/com/florianbardin/vitisapi/winery/dto/WineryDto.java
+++ src/main/java/com/florianbardin/vitisapi/winery/dto/WineryDto.java
@@ -1,13 +1,20 @@
 package com.florianbardin.vitisapi.winery.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record WineryDto(
         Integer id,
-        @NotBlank
+
+        @NotBlank(message = "Name is required")
+        @Size(max = 255, message = "Name must be less than 255 characters")
         String name,
-        @NotBlank
+
+        @NotBlank(message = "Region is required")
+        @Size(max = 100, message = "Region must be less than 100 characters")
         String region,
+
+        @Size(max = 255, message = "Address must be less than 255 characters")
         String address
 ) {
 }
