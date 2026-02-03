f40c920238b53ae49b4e3572d7f67916ce60bde4
Florian Bardin <fbardin51@gmail.com>
2026-02-03 23:52:34 +0100

    feat(winery-specs): added JpaSpecificationExecutor to winery repository to accept specifications

--- src/main/java/com/florianbardin/vitisapi/winery/WineryRepository.java
+++ src/main/java/com/florianbardin/vitisapi/winery/WineryRepository.java
@@ -1,6 +1,7 @@
 package com.florianbardin.vitisapi.winery;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface WineryRepository extends JpaRepository<Winery, Integer> {
+public interface WineryRepository extends JpaRepository<Winery, Integer>, JpaSpecificationExecutor<Winery> {
 }
