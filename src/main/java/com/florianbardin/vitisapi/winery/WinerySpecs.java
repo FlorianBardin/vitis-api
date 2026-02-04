54e2001b978c29505d8d7c0317c56c5afed14c43
Florian Bardin <fbardin51@gmail.com>
2026-02-04 23:28:54 +0100

    feat(winery-specs): added static method to find wineries by containing letters in the name

--- src/main/java/com/florianbardin/vitisapi/winery/WinerySpecs.java
+++ src/main/java/com/florianbardin/vitisapi/winery/WinerySpecs.java
@@ -5,6 +5,17 @@ import org.springframework.data.jpa.domain.Specification;
 public class WinerySpecs {
     public static Specification<Winery> hasRegion(String region) {
         return (root, query, criteriaBuilder) ->
-                criteriaBuilder.equal(root.get("region"), region);
+                criteriaBuilder.equal(
+                        criteriaBuilder.lower(root.get("region")),
+                        region.toLowerCase()
+                );
+    }
+
+    public static Specification<Winery> nameContains(String name) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.like(
+                        criteriaBuilder.lower(root.get("name")),
+                        "%" +  name.toLowerCase() + "%"
+                );
     }
 }
